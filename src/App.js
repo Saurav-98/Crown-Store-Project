bfdd0c355917daeee9507d4fbbaf86e4ede6b876
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-07-19 09:58:12 +0530

    Initialised Project and implement basic style

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import "./App.css";
 
+import "./Categories.scss";
+
 const categories = [
   {
-    title: "Hats",
-    id: "001",
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
   },
   {
-    title: "Jackets",
-    id: "002",
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
   },
   {
-    title: "Sneakers",
-    id: "003",
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
   },
   {
-    title: "Mens",
-    id: "004",
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
   },
   {
-    title: "Womens",
-    id: "005",
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
   },
 ];
-
 function App() {
   return (
     <div className="categories-container">
-      {categories.map(({ id, title }) => (
+      {categories.map(({ id, title, imageUrl }) => (
         <div key={id} className="category-container">
-          {/* IMG */}
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
           <div className="category-info-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
